62e30ae9551d1bdd9f78daf3055385c4654d1012
Dave Keen <dev@ruffness.com>
2015-03-19 14:03:34 +0100

    Added support for log levels

--- log.js
+++ log.js
@@ -1,162 +1,121 @@
 (function() {
-  var exportedLog, ffSupport, formats, getOrderedMatches, hasMatches, isFF, isIE, isOpera, isSafari, log, makeArray, operaSupport, safariSupport, stringToArgs, _log;
+	"use strict";
 
-  if (!(window.console && window.console.log)) {
-    return;
-  }
+	var formats, getOrderedMatches, hasMatches, makeArray, stringToArgs, outputMessage;
+	var log, info, debug, warn, error;
 
-  log = function() {
-    var args;
-    args = [];
-    makeArray(arguments).forEach(function(arg) {
-      if (typeof arg === 'string') {
-        return args = args.concat(stringToArgs(arg));
-      } else {
-        return args.push(arg);
-      }
-    });
-    return _log.apply(window, args);
-  };
+	if (!(window.console && window.console.log)) {
+		return;
+	}
 
-  _log = function() {
-    return console.log.apply(console, makeArray(arguments));
-  };
+	outputMessage = function(level, logArgs) {
+		var args = [];
+		makeArray(logArgs).forEach(function(arg) {
+			if (typeof arg === 'string') {
+				return args = args.concat(stringToArgs(arg));
+			} else {
+				return args.push(arg);
+			}
+		});
 
-  makeArray = function(arrayLikeThing) {
-    return Array.prototype.slice.call(arrayLikeThing);
-  };
+		return console[level].apply(console, makeArray(args));
+	};
 
-  formats = [
-    {
-      regex: /\*([^\*]+)\*/,
-      replacer: function(m, p1) {
-        return "%c" + p1 + "%c";
-      },
-      styles: function() {
-        return ['font-style: italic', ''];
-      }
-    }, {
-      regex: /\_([^\_]+)\_/,
-      replacer: function(m, p1) {
-        return "%c" + p1 + "%c";
-      },
-      styles: function() {
-        return ['font-weight: bold', ''];
-      }
-    }, {
-      regex: /\`([^\`]+)\`/,
-      replacer: function(m, p1) {
-        return "%c" + p1 + "%c";
-      },
-      styles: function() {
-        return ['background: rgb(255, 255, 219); padding: 1px 5px; border: 1px solid rgba(0, 0, 0, 0.1)', ''];
-      }
-    }, {
-      regex: /\[c\=(?:\"|\')?((?:(?!(?:\"|\')\]).)*)(?:\"|\')?\]((?:(?!\[c\]).)*)\[c\]/,
-      replacer: function(m, p1, p2) {
-        return "%c" + p2 + "%c";
-      },
-      styles: function(match) {
-        return [match[1], ''];
-      }
-    }
-  ];
+	makeArray = function(arrayLikeThing) {
+		return Array.prototype.slice.call(arrayLikeThing);
+	};
 
-  hasMatches = function(str) {
-    var _hasMatches;
-    _hasMatches = false;
-    formats.forEach(function(format) {
-      if (format.regex.test(str)) {
-        return _hasMatches = true;
-      }
-    });
-    return _hasMatches;
-  };
+	formats = [
+		{
+			regex: /\*([^\*]+)\*/,
+			replacer: function(m, p1) {
+				return "%c" + p1 + "%c";
+			},
+			styles: function() {
+				return ['font-style: italic', ''];
+			}
+		}, {
+			regex: /\_([^\_]+)\_/,
+			replacer: function(m, p1) {
+				return "%c" + p1 + "%c";
+			},
+			styles: function() {
+				return ['font-weight: bold', ''];
+			}
+		}, {
+			regex: /\`([^\`]+)\`/,
+			replacer: function(m, p1) {
+				return "%c" + p1 + "%c";
+			},
+			styles: function() {
+				return ['background: rgb(255, 255, 219); padding: 1px 5px; border: 1px solid rgba(0, 0, 0, 0.1)', ''];
+			}
+		}, {
+			regex: /\[c\=(?:\"|\')?((?:(?!(?:\"|\')\]).)*)(?:\"|\')?\]((?:(?!\[c\]).)*)\[c\]/,
+			replacer: function(m, p1, p2) {
+				return "%c" + p2 + "%c";
+			},
+			styles: function(match) {
+				return [match[1], ''];
+			}
+		}
+	];
 
-  getOrderedMatches = function(str) {
-    var matches;
-    matches = [];
-    formats.forEach(function(format) {
-      var match;
-      match = str.match(format.regex);
-      if (match) {
-        return matches.push({
-          format: format,
-          match: match
-        });
-      }
-    });
-    return matches.sort(function(a, b) {
-      return a.match.index - b.match.index;
-    });
-  };
+	hasMatches = function(str) {
+		var _hasMatches;
+		_hasMatches = false;
+		formats.forEach(function(format) {
+			if (format.regex.test(str)) {
+				return _hasMatches = true;
+			}
+		});
+		return _hasMatches;
+	};
 
-  stringToArgs = function(str) {
-    var firstMatch, matches, styles;
-    styles = [];
-    while (hasMatches(str)) {
-      matches = getOrderedMatches(str);
-      firstMatch = matches[0];
-      str = str.replace(firstMatch.format.regex, firstMatch.format.replacer);
-      styles = styles.concat(firstMatch.format.styles(firstMatch.match));
-    }
-    return [str].concat(styles);
-  };
+	getOrderedMatches = function(str) {
+		var matches;
+		matches = [];
+		formats.forEach(function(format) {
+			var match;
+			match = str.match(format.regex);
+			if (match) {
+				return matches.push({
+					format: format,
+					match: match
+				});
+			}
+		});
+		return matches.sort(function(a, b) {
+			return a.match.index - b.match.index;
+		});
+	};
 
-  isSafari = function() {
-    return /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-  };
+	stringToArgs = function(str) {
+		var firstMatch, matches, styles;
+		styles = [];
+		while (hasMatches(str)) {
+			matches = getOrderedMatches(str);
+			firstMatch = matches[0];
+			str = str.replace(firstMatch.format.regex, firstMatch.format.replacer);
+			styles = styles.concat(firstMatch.format.styles(firstMatch.match));
+		}
+		return [str].concat(styles);
+	};
 
-  isOpera = function() {
-    return /OPR/.test(navigator.userAgent) && /Opera/.test(navigator.vendor);
-  };
+	var exports = {
+		log: function() { return outputMessage.apply(this, ["log", arguments]); },
+		debug: function() { return outputMessage.apply(this, ["debug", arguments]); },
+		info: function() { return outputMessage.apply(this, ["info", arguments]); },
+		warn: function() { return outputMessage.apply(this, ["warn", arguments]); },
+		error: function() { return outputMessage.apply(this, ["error", arguments]); }
+	};
 
-  isFF = function() {
-    return /Firefox/.test(navigator.userAgent);
-  };
-
-  isIE = function() {
-    return /MSIE/.test(navigator.userAgent);
-  };
-
-  safariSupport = function() {
-    var m;
-    m = navigator.userAgent.match(/AppleWebKit\/(\d+)\.(\d+)(\.|\+|\s)/);
-    if (!m) {
-      return false;
-    }
-    return 537.38 <= parseInt(m[1], 10) + (parseInt(m[2], 10) / 100);
-  };
-
-  operaSupport = function() {
-    var m;
-    m = navigator.userAgent.match(/OPR\/(\d+)\./);
-    if (!m) {
-      return false;
-    }
-    return 15 <= parseInt(m[1], 10);
-  };
-
-  ffSupport = function() {
-    return window.console.firebug || window.console.exception;
-  };
-
-  if (isIE() || (isFF() && !ffSupport()) || (isOpera() && !operaSupport()) || (isSafari() && !safariSupport())) {
-    exportedLog = _log;
-  } else {
-    exportedLog = log;
-  }
-
-  exportedLog.l = _log;
-
-  if (typeof define === 'function' && define.amd) {
-    define(function defineLog() {
-      return exportedLog;
-    });
-  } else if (typeof exports !== 'undefined') {
-    module.exports = exportedLog;
-  } else {
-    window.log = exportedLog;
-  }
+	if (typeof define === 'function' && define.amd) {
+		define(function defineLog() {
+			return exports;
+		});
+	} else if (typeof exports !== 'undefined') {
+		module.exports = exports;
+	}
 
 }).call(this);
